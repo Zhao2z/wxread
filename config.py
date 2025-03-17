086666b538bef717d4c26c2fa03c8b8c6f36b734
Zhao2z <141019403+Zhao2z@users.noreply.github.com>
2025-03-17 12:08:55 +0800

    ..

--- config.py
+++ config.py
@@ -95,25 +95,25 @@ data = {
 }
 
 
-def convert(curl_command):
-    """提取bash接口中的headers与cookies"""
-    headers = {}
+# def convert(curl_command):
+#     """提取bash接口中的headers与cookies"""
+#     headers = {}
 
-    # 提取 headers
-    for match in re.findall(r"-H '([^:]+): ([^']+)'", curl_command):
-        headers[match[0].strip()] = match[1].strip()
+#     # 提取 headers
+#     for match in re.findall(r"-H '([^:]+): ([^']+)'", curl_command):
+#         headers[match[0].strip()] = match[1].strip()
 
-    # 提取 cookies（支持 -b 传递）
-    cookies = {}
-    cookie_match = re.search(r"-b '([^']+)'", curl_command)
-    if cookie_match:
-        cookie_string = cookie_match.group(1)
-        for cookie in cookie_string.split('; '):
-            if '=' in cookie:
-                key, value = cookie.split('=', 1)
-                cookies[key] = value
+#     # 提取 cookies（支持 -b 传递）
+#     cookies = {}
+#     cookie_match = re.search(r"-b '([^']+)'", curl_command)
+#     if cookie_match:
+#         cookie_string = cookie_match.group(1)
+#         for cookie in cookie_string.split('; '):
+#             if '=' in cookie:
+#                 key, value = cookie.split('=', 1)
+#                 cookies[key] = value
 
-    return headers, cookies
+#     return headers, cookies
 
 
 
